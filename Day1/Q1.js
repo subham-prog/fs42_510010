a43339e3e9e67ad706202a01236e0ede34b0da22
subham-prog <sdhali330@gmail.com>
2025-01-05 23:40:42 +0530

    Q2

--- Day1/Q1.js
+++ Day1/Q1.js
@@ -1,3 +1,3 @@
-let room_temp = [20, 21, 22, 23, 24, 25, 26, 27];
-room_temp[3] = room_temp[3] + 2;
+let room_temp = [20, 21, 22, 23, 24, 25, 26];  // day 1 to day 7 daily room temperature
+room_temp[3] = room_temp[3] + 2;  // updated room temperature of 4th day by 2 degree 
 console.log(room_temp);
---
+++ Day1/Q2.js
@@ -0,0 +1,4 @@
+let todo = ["eat", "brush", "read", "write", "sleep"];
+todo.unshift("Shower");
+todo.pop();
+console.log(todo)
