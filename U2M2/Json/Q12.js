aad5011cef74d6594c2676f7cfeda17237853a44
subham-prog <sdhail330@gmail.com>
2025-06-27 03:02:21 +0530

    New

---
+++ U2M2/Json/Q12.js
@@ -0,0 +1,17 @@
+function filternumber(arr){
+    let filtered = arr.filter(num => num % 2 === 0);
+    console.log(filtered)
+  }
+  
+  function sumOfArray(arr) {
+    let sum = arr.reduce((acc, curr) => acc + curr, 0);
+    console.log(sum)
+  }
+  function sortAndConcat(arr1, arr2) {
+    const sorted1 = arr1.slice().sort((a, b) => a - b);
+    const sorted2 = arr2.slice().sort((a, b) => a - b);
+    console.log(sorted1.concat(sorted2))
+  }
+  sortAndConcat([10, 12, 1, 2, 3, 4, 9], [2, 4, 5, 6, 8, 9, 10])
+  filternumber([10, 12, 1, 2, 3, 4, 9])
+  sumOfArray([10, 12, 1, 2, 3, 4, 9])
